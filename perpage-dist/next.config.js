f92329dc74bd3d56625cd30ea02c0f0b3a4efb5b
J <44283589+jinny1010@users.noreply.github.com>
2026-01-12 21:32:50 +0900

    Delete perpage-dist directory

--- perpage-dist/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'www.notion.so', 'prod-files-secure.s3.us-west-2.amazonaws.com'],
-  },
-}
-
-module.exports = nextConfig
